5a06bbb472b5c606b403218df2d2ef19cb1c0019
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-02-03 23:15:12 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -12,12 +12,20 @@ function App() {
         { id: 1, firstName: "Pera", lastName: "Peric", phone: "11-11-11-11", email: "pera123@pera.com" },
         { id: 2, firstName: "Laza", lastName: "Lazic", phone: "22-22-22-22", email: "laza123@laza.com" },
         { id: 3, firstName: "Kosta", lastName: "Kostic", phone: "33-33-33-33", email: "kosta123@kosta.com" }
-    ])
+    ]);
 
     const addNewAccount = acc => {
         acc.id = account.length + 1;
         setAccount(prevAccount => prevAccount.concat(acc));
     }
+
+    const saveEditedAccount = acc => {
+        const accountForEdit = account.filter(el => el.id == acc.id)[0];
+        const index = account.indexOf(accountForEdit);
+        const editedAccount = Object.assign(accountForEdit, acc);
+        const accountCopy = account;
+        accountCopy[index] = editedAccount;
+    };
 
     const deleteAccount = idDeletedAccount => setAccount(prevAccount => prevAccount = prevAccount.filter(el => el.id !== idDeletedAccount));
 
@@ -32,11 +40,11 @@ function App() {
                     <AddAccount addNewAccount={addNewAccount} />
                 </Route>
                 <Switch>
+                    <Route path="/edit/:id">
+                        <EditAccount listOfAccounts={account} saveEditedAccount={saveEditedAccount} />
+                    </Route>
                     <Route path="/edit-delete">
                         <EditDeleteTable account={account} deleteAccount={deleteAccount} />
-                    </Route>
-                    <Route path="/edit">
-                        <EditAccount account={account}/>
                     </Route>
                 </Switch>
             </BrowserRouter>
--- src/components/EditDeleteTable/EditAccount/EditAccount.js
+++ src/components/EditDeleteTable/EditAccount/EditAccount.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withRouter, useParams, useHistory } from "react-router-dom";
 import "./EditAccount.css";
 
-function EditAccount({ account }) {
-    console.log(account);
+function EditAccount({ listOfAccounts, saveEditedAccount }) {
+
+    const [account, setAccount] = useState({});
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        const accountForEdit = listOfAccounts.filter(el => el.id == id)[0];
+        document.querySelector("#firstName").value = accountForEdit.firstName;
+        document.querySelector("#lastName").value = accountForEdit.lastName;
+        document.querySelector("#phone").value = accountForEdit.phone;
+        document.querySelector("#email").value = accountForEdit.email;
+    }, []);
+
+    const editAccount = e => {
+        const editedAcc = Object.assign(account, { id: Number(id), [e.target.id]: e.target.value });
+        setAccount(prevAccount => prevAccount = prevAccount = editedAcc);
+    };
+
+    const history = useHistory();
+
+    const saveAccount = () => {
+        saveEditedAccount(account);
+        history.push("/edit-delete");
+    }
+
     return (
         <div>
             <div className="add-block">
                 <div className="add-text">Edit Account</div>
                 <div className="inputs">
-                    <input type="text" id="firstName" placeholder="First Name" className="input-field" /><br /><br />
-                    <input type="text" id="lastName" placeholder="Last Name" className="input-field" /><br /><br />
-                    <input type="text" id="phone" placeholder="Phone" className="input-field" /><br /><br />
-                    <input type="text" id="email" placeholder="Email" className="input-field" /><br /><br />
-                    <button>Save</button>
+                    <input type="text" id="firstName" placeholder="First Name" className="input-field" onChange={editAccount} /><br /><br />
+                    <input type="text" id="lastName" placeholder="Last Name" className="input-field" onChange={editAccount} /><br /><br />
+                    <input type="text" id="phone" placeholder="Phone" className="input-field" onChange={editAccount} /><br /><br />
+                    <input type="text" id="email" placeholder="Email" className="input-field" onChange={editAccount} /><br /><br />
+                    <button onClick={() => saveAccount()}>Save</button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default EditAccount;
+export default withRouter(EditAccount);
